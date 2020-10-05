545c190c89da71a14af1244e84a7fc6929ffaf54
Thomas Mair <thomas.mair@Thomass-MacBook-Pro.local>
2020-10-05 11:28:33 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// @ts-check
+
+function init() {
+    var websocket = new WebSocket("ws://127.0.0.1:8080");
+  
+    websocket.onopen = function() {
+      document.getElementById("output").innerHTML += "<p>> CONNECTED</p>";
+    };
+  
+    websocket.onmessage = function(evt) {
+      document.getElementById("output").innerHTML +=
+        "<p style='color: blue;'>> RESPONSE: " + evt.data + "</p>";
+    };
+  
+    /**
+     * 
+     * @param {any} evt 
+     */
+    websocket.onerror = function(evt) {
+      document.getElementById("output").innerHTML +=
+        "<p style='color: red;'>> ERROR: " + evt.data + "</p>";
+    };
+  
+    /**
+     * @type {HTMLInputElement}
+     */
+    var inputField = document.getElementById("input");
+  
+    inputField.addEventListener("keypress", /** @param {KeyboardEvent} event */(event) => {
+      if (inputField.value) {
+        if (event.keyCode == 13) {
+          sendMessage(inputField.value);
+          inputField.value = null;
+        }
+      }
+    });
+    function sendMessage(message) {
+      document.getElementById("output").innerHTML +=
+        "<p>> SENT: " + message + "</p>";
+  
+      websocket.send(message);
+    }
+  }
+  
+  init();
+  
